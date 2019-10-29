17650abfe51f681a8d58943b706c378e9e17dc70
lkoniari <56820684+lkoniari@users.noreply.github.com>
2019-10-29 23:28:00 +0200

    Add files via upload

---
+++ W2D2Ex2.java
@@ -0,0 +1,22 @@
+
+package w2d2ex2;
+
+public class W2D2Ex2 {
+
+    public static void main(String[] args) {
+        final double kmToNm=1.852;
+        final double milesToNm=1.150779;
+        
+        double nMiles;
+        nMiles = 36;
+        
+        double nmilesToKm = nMiles*kmToNm;
+        
+        System.out.println(nMiles + " nautical miles are "+nmilesToKm+" kilometers");
+        
+        /*1 nautical mile = 1.852 km . given the number of nautical miles
+        all we have to do is to multiply that number with 1.852*/
+   
+    }
+    
+}
